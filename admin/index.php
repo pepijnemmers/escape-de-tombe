e43b2b0d9ed05c165c52a1c9392f4cdf66b97212
pepijnemmers <pepijn.emmers@edu-kw1c.nl>
2022-10-14 15:28:33 +0200

    changed title

--- admin/index.php
+++ admin/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reserveren - Escape Room KW1C</title>
+    <title>Admin - Escape Room KW1C</title>
     <link rel="stylesheet" href="../styles/styles.css">
     <script src="../js/script.js" defer></script>
     <script src="../js/index.js" defer></script>
