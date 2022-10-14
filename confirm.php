ae039e6644808e5bd87284b4954a5f07ee1c5571
Quinten Kornalijnslijper <quinten.kornalijnslijper@edu-kw1c.nl>
2022-10-14 12:54:37 +0200

    Confirm.php

--- confirm.php
+++ confirm.php
@@ -1,9 +1,57 @@
 <?php
+$serverName = "reseveringsysteem.database.windows.net";
+$connectionOptions = array(
+    "Database" => "Reserveringssysteem",
+    "Uid" => "Admin123",
+    "PWD" => "R070507038!"
+);
 
-// php insert in database
-    // gebruik geen mysql want de azure database is microsoft sql db :)
+//Establishes the connection
+{
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "INSERT INTO Groep(Naam) VALUES (?)";
+    $params = array($_POST["groupsname"]);
+    $stmt = sqlsrv_query($conn, $tsql, $params);
+    if( $stmt === false ) {
+        die( print_r( sqlsrv_errors(), true));
+    }
+}
+{
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT * FROM Groep WHERE Naam = ?";
+    $params = array($_POST["groupsname"]);
+    $getResults= sqlsrv_query($conn, $tsql, $params);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
 
-// redirect to `bevestiging.php`
-header("Location: bevestiging");
+    $row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC);  
+    
+    $tsql = "INSERT INTO GroepTijdslot(GroepId, TijdslotId) VALUES (?, ?)";
+    $params = array($row['GroepId'], $_POST["time"]);
+    $stmt = sqlsrv_query($conn, $tsql, $params);
+    if( $stmt === false ) {
+        die( print_r( sqlsrv_errors(), true));
+    }
+}
+{
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT * FROM Groep WHERE Naam = ?";
+    $params = array($_POST["groupsname"]);
+    $getResults= sqlsrv_query($conn, $tsql, $params);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+
+    $row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC);  
+
+    for($i = 0; $i < $_POST["nrStudents"]; $i++)
+    {
+        $tsql = "INSERT INTO Student(Studentnummer, Naam, Email, GroepId) VALUES (?, ?, ?, ?)";
+        $params = array($_POST["student" . $i + 1 . "Nr"], $_POST["student" . $i + 1 . "Name"], $_POST["student" . $i + 1 . "Email"], $row['GroepId']);
+        $stmt = sqlsrv_query($conn, $tsql, $params);
+        if( $stmt === false ) {
+            die( print_r( sqlsrv_errors(), true));
+        }
+    }
+}
 
 ?>
