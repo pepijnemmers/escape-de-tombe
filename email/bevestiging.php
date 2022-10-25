e183237c13d06893958f16ed625c2e1774e938ae
pepijnemmers <pepijn.emmers@edu-kw1c.nl>
2022-10-25 16:42:51 +0200

    email ui

---
+++ email/bevestiging.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!isset($_GET["group"])) {
+    header("Location: /");
+}
+
+$mail = file_get_contents("../email.php");
+$mail = str_replace("Bekijk online versie", "", $mail);
+$mail = str_replace("{{Groepsnaam}}", $_GET["group"], $mail);
+$mail = str_replace("{{Datum}}", $_GET["datum"], $mail);
+$mail = str_replace("{{Lokaal}}", $_GET["lokaal"], $mail);
+
+$mail = str_replace('<div class="es-wrapper-color" style="', '<div class="es-wrapper-color" style="min-height:100vh;', $mail);
+
+echo $mail;
+
+?>
