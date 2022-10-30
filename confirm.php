750346206ebc0b72055c6effc791fb2fa41ca8fe
pepijnemmers <pepijn.emmers@edu-kw1c.nl>
2022-10-30 17:15:59 +0100

    fixed date and time

--- confirm.php
+++ confirm.php
@@ -99,11 +99,25 @@ catch (Exception $e) {
     header("Location: /?error=insert#aanmelden");
 }
 
+try {
+    $sql = "SELECT * FROM `tijdslot` WHERE `TijdslotId` = '". $_POST["time"] ."'";
+    $resultTijd = $conn->query($sql);
+
+    if ($resultTijd->num_rows > 0) {
+        $tijdRow = $resultTijd->fetch_assoc();
+    }
+    $tijdString = $tijdRow["Tijd"];
+    $tijdString = substr($tijdString, 0, -3);
+    $tijdString = substr($tijdString, 11);
+} catch (Exception $e) {
+    $tijdString = "Onbekend";
+}
+
 
 // getting email html code and inserting variables
 $Groepsnaam = $_POST["groupsname"];
-$Tijd = $_POST["time"];
-$Datum = "15-11-2022 " . $_POST["time"];
+$Tijd = $tijdString; // $_POST["time"];
+$Datum = "15-11-2022 " . $Tijd;
 $Lokaal = "OE1250";
 $LinkOnline = "https://escapedetombe.nl/email/bevestiging?group=$Groepsnaam&datum=$Tijd&lokaal=$Lokaal";
 
